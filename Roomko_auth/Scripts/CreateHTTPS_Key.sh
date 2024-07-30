c7766c99991408f8fdd8a5288240087355e031b9
killi1812 <cvok.fran@gmail.com>
2024-07-30 10:36:27 +0200

    Setup https script

--- Roomko_auth/Scripts/CreateHTTPS_Key.sh
+++ Roomko_auth/Scripts/CreateHTTPS_Key.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+# Check if the build directory exists
+if [ ! -d "./../build" ]; then
+  mkdir ./build
+  echo "Created directory: build"
+fi
 
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout ./../build/keys/Https_key.pem -out ./../build/keys/Https_cert.pem
+# Check if the build/keys directory exists
+if [ ! -d "./../build/keys" ]; then
+  mkdir ./build/keys
+  echo "Created directory: build/keys"
+fi
+
+# TODO create them even though they exist
+if [ ! -d "./../build/keys/Https_key.pem" ]; then
+  echo "Creating HTTPS key"
+  openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout ./../build/keys/Https_key.pem -out ./../build/keys/Https_cert.pem
+fi
+
